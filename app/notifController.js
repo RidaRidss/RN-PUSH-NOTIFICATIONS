399aa7da4ca33c686fb0f25467a974823f1e8254
RidaRidss <rida_rocks12@yahoo.com>
2018-07-16 12:52:34 +0500

    notification created when change date n time

---
+++ app/notifController.js
@@ -0,0 +1,63 @@
+import React, { Component } from "react";
+import PushNotification from "react-native-push-notification";
+import moment from "moment";
+import localization from "moment/locale/hu";
+
+const NOTIFICATION_DATE_TIME_FORMAT = "YYYY-MM-DD HH:mm";
+
+export default class notifController extends Component {
+  componentDidMount() {
+    PushNotification.configure({
+      onNotification: function(notification) {
+        console.log("NOTIFICATION: ", notification);
+      },
+      popInitialNotification: true
+    });
+  }
+
+  render() {
+    return null;
+  }
+}
+
+export function scheduleLocalNotification(message, date, id, payload) {
+  // message: type String
+  // date: type String  format 'YYYY-MM-DD HH:mm' (NOTIFICATION_DATE_TIME_FORMAT)
+
+  //construct the notification parameters
+  const fireDate = moment(date, NOTIFICATION_DATE_TIME_FORMAT).toDate();
+  const notification = {
+    id: createPushId(id), //for android cancel notification (must be stringified number)
+    message, // (required)
+    date: fireDate,
+    data: JSON.stringify(payload),
+    vibrate: true, // (optional) default: true
+    vibration: 300, // vibration length in milliseconds, ignored if vibrate=false, default: 1000
+    tag: "#my_sleep_time", // (optional) add tag to message
+    ongoing: true, // (optional) set whether this is an "ongoing" notification
+
+    /* iOS and Android properties */
+    title: "Reminder", // (optional, for iOS this is only used in apple watch, the title will be the app name on other iOS devices)
+    permissions: {
+      alert: true,
+      badge: true,
+      sound: true
+    },
+    playSound: true, // (optional) default: true
+    color: "green", // (optional) default: system default
+    soundName: "default"
+  };
+
+  //schedule the notification
+  PushNotification.localNotificationSchedule(notification);
+}
+
+export function createPushId(pushType) {
+  return NOTIFICATION_TYPE_TO_ID[pushType];
+}
+
+export const NOTIFICATION_TYPE_TO_ID = {
+  logs: "0",
+  rems: "1",
+  ins: "2"
+};
