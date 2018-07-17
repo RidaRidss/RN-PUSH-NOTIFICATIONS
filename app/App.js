4797bb15ca9e50c1f5381a810f7f11c20bc99975
RidaSarwar <rsarwar@digital-dividend.com>
2018-07-17 23:34:25 +0500

    added date n time picker , choose date n time and set notifications on your choice :)

--- app/App.js
+++ app/App.js
@@ -8,11 +8,51 @@ import PushController, {
 import PushNotification from "react-native-push-notification";
 import DatePicker from "react-native-datepicker";
 
+// getting current time
+// ======================
+
+var date = new Date();
+var hours = date.getHours();
+var minutes = date.getMinutes();
+var ampm = hours >= 12 ? "pm" : "am";
+var hours = (date.getHours() < 10 ? "0" : "") + date.getHours();
+var minutes = (date.getMinutes() < 10 ? "0" : "") + date.getMinutes();
+var strTime = hours + ":" + minutes + " " + ampm;
+
+// ================================================================================================
+
+// arrange a (current date n picked time from picker) format to pass as param for push notification
+// ================================================================================================
+
+var getYear = date.getFullYear();
+var getMonth = date.getMonth() + 1;
+var getDate = date.getDate();
+var time = strTime;
+var hours = Number(time.match(/^(\d+)/)[1]);
+var minutes = Number(time.match(/:(\d+)/)[1]);
+var AMPM = time.match(/\s(.*)$/)[1];
+if (AMPM == "PM" && hours < 12) hours = hours + 12;
+if (AMPM == "AM" && hours == 12) hours = hours - 12;
+var sHours = hours.toString();
+var sMinutes = minutes.toString();
+if (hours < 10) sHours = "0" + sHours;
+if (minutes < 10) sMinutes = "0" + sMinutes;
+
+// ================================================================================================
+
+// getting current Date
+
+// ====================
+
+var currentDate = getYear + "-" + getMonth + "-" + getDate;
+
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { datetime: "2016-05-15 16:04" };
-
+    this.state = {
+      time: strTime,
+      date: currentDate
+    };
     this.handleAppStateChange = this.handleAppStateChange.bind(this);
   }
 
@@ -37,40 +77,49 @@ export default class App extends Component {
   }
 
   render() {
+    var timePickedNDate = this.state.date + " " + this.state.time;
+    console.log(timePickedNDate);
     return (
       <View style={styles.container}>
         <Text style={styles.welcome}>PUSH NOTIFICATIONS</Text>
-        // <Button
-        //   title="Press here to schedule a notification"
-        //   // onPress={() =>
-        //   //   // scheduleLocalNotification(
-        //   //   //   "I am your notification",
-        //   //   //   this.state.datetime,
-        //   //   //   "logs",
-        //   //   //   {
-        //   //   //     test: "1"
-        //   //   //   }
-        //   //   // )
-        //   // }
-        // />
-        <DatePicker
-          style={{ width: 200 }}
-          date={this.state.datetime}
-          mode="datetime"
-          format="YYYY-MM-DD HH:mm"
-          confirmBtnText="Confirm"
-          cancelBtnText="Cancel"
-          showIcon={false}
-          onDateChange={datetime => {
-            this.setState({ datetime: datetime });
+        <Button
+          style={{
+            marginBottom: 10
+          }}
+          title="Set Date & Time & Press here to schedule a notification"
+          onPress={() =>
             scheduleLocalNotification(
-              "I am your notification",
-              this.state.datetime,
+              "Change Time & you will get a push notification on current date n picked time",
+              timePickedNDate,
               "logs",
               {
                 test: "1"
               }
-            );
+            )
+          }
+        />
+        <DatePicker
+          style={{ width: 200 }}
+          date={this.state.time}
+          mode="time"
+          format="hh:mm A"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          showIcon={false}
+          onDateChange={time => {
+            this.setState({ time: time });
+          }}
+        />
+        <DatePicker
+          style={{ width: 200, marginTop: 10 }}
+          date={this.state.date}
+          mode="date"
+          format="YYYY-MM-DD"
+          confirmBtnText="Confirm"
+          cancelBtnText="Cancel"
+          showIcon={false}
+          onDateChange={date => {
+            this.setState({ date: date });
           }}
         />
         <PushController />
